c7fcca797b7a9dd86f9eba0f07f3dca1b51fffa4
marlo <narl0durn0@ya.ru>
2022-06-17 11:40:00 +0300

    another solution of problem

--- temp/code.py
+++ temp/code.py
@@ -6,17 +6,19 @@ commands = [
     lambda x: x + 1,
     lambda x: x * 2 + 1,
 ]
-counter = 0
 
-def solution(num: int, match: int, exclude: int):
-    global counter
-    if num == exclude or num > match:
-        return None
-    if num == match:
-        counter += 1
-    for i in range(2):
-        solution(commands[i](num), match, exclude)
+def solution(num, match: int, exclude: int) -> int:
+    counter = 0
+    def inner(num: int, match: int, exclude: int):
+        nonlocal counter
+        if num == exclude or num > match:
+            return None
+        if num == match:
+            counter += 1
+        for i in range(2):
+            inner(commands[i](num), match, exclude)
+    inner(num, match, exclude)
+    return counter
 
 
-solution(1, 27, 26)
-print(counter)
+print(solution(1, 27, 26))
